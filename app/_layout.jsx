ff77577c7d4a4c3893e0c83f399c606af23e53fc
iamdevcode <iamdevcode@gmail.com>
2024-07-17 12:31:14 -0400

    add tailwind

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -7,7 +7,7 @@ SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf')
+    SpaceMono: require('../src/assets/fonts/SpaceMono-Regular.ttf')
   })
 
   useEffect(() => {
@@ -23,7 +23,6 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="+not-found" />
     </Stack>
   )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: ['./app/**/*.{,jsx,}', './src/**/*.{jsx,}'],
+  theme: {
+    extend: {
+      colors: {}
+    }
+  },
+  plugins: []
+}
